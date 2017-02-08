9b31ecddcfb8054369e556023206b85a10672a46
Agustin De Leon <agustinslions@gmail.com>
2017-02-07 21:42:13 -0300

    Create screens for onboarding and settings

---
+++ ManageStoryboards/ViewControllers/SettingsViewControllers/SL_ChangeImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  SL_ChangeImageViewController.h
+//  ManageStoryboards
+//
+//  Created by Agustin De Leon on 7/2/17.
+//  Copyright © 2017 Agustin De Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SL_ChangeImageViewController : UIViewController
+
+@end
